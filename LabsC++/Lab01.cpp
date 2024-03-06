4377900ccc284a2dcd96169604c66c05a6955bf8
Karina Melendez <kmelendez@arecibo.inter.edu>
2024-03-05 21:53:29 -0400

    new file

---
+++ LabsC++/Lab01.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int entero;
+    float flotante;
+    char caracter;
+    string cadena;
+    
+    cout << "Ingrese un número entero: ";
+    cin >> entero;
+    
+    cout << "Ingrese un número flotante: ";
+    cin >> flotante;
+
+    cout << "Ingrese un carácter: ";
+    cin >> caracter;
+
+    cout << "Ingrese una cadena de caracteres: ";
+    cin.ignore();  // ignorar el '\n' dejado por
+                  // std::cin >> caracter
+    getline(cin, cadena);
+
+    return 0;
+}
