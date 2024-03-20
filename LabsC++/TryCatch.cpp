c087395cfa098db1c6cf81ad7eaf817b915a8cd3
Karina Melendez <kmelendez@arecibo.inter.edu>
2024-03-19 21:27:16 -0400

    new files

--- LabsC++/TryCatch.cpp
+++ LabsC++/TryCatch.cpp
@@ -1,14 +1,15 @@
 
-Para mostrar un ejemplo sencillo de cómo usar try y catch en C++ a los estudiantes, podemos crear un programa que solicite al usuario que ingrese dos números y luego intente dividir el primer número por el segundo. Utilizaremos un bloque try para intentar la operación de división y un bloque catch para manejar el caso en el que el usuario intente dividir por cero, lo cual no es permitido en matemáticas y causaría un error en tiempo de ejecución.
+/*Para mostrar un ejemplo sencillo de cómo usar try y catch en C++ a los estudiantes, 
+podemos crear un programa que solicite al usuario que ingrese dos números y 
+luego intente dividir el primer número por el segundo. Utilizaremos un bloque try 
+para intentar la operación de división y un bloque catch para manejar el caso en el que el usuario intente dividir por cero, lo cual no es permitido en matemáticas y causaría un error en tiempo de ejecución.
+*/
 
-Aquí tienes el código de ejemplo:
-
-cpp
-Copy code
 #include <iostream>
 using namespace std;
 
-int main() {
+int main() 
+{
     int numerador, denominador, resultado;
 
     cout << "Ingrese el numerador: ";
@@ -21,7 +22,7 @@ int main() {
     {
         if (denominador == 0) 
         {
-            throw "División por cero no permitida.";
+            throw "División por cero no permitida."; //mensaje que describe el problema
         }
         resultado = numerador / denominador;
 
